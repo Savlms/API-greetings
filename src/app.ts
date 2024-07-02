f7c33f1e4dc644fdf0fa7221da4699ba9afdf695
Savlms <valmarnas@gmail.com>
2024-07-02 12:43:01 -0700

    final update

--- src/app.ts
+++ src/app.ts
@@ -3,57 +3,61 @@ import geoip from 'geoip-lite';
 import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config()
-
 const app = express();
 const port = process.env.PORT;
 console.log()
-
-// Replace 'YOUR_API_KEY' with your actual OpenWeatherMap API key
-const weatherApiKey = process.env.weatherAPIKEY;
+const ipApiKey = process.env.ipApiKey;
+const weatherApiKey = process.env.weatherApiKey
 
 app.get('/api/hello', async (req: Request, res: Response) => {
     const client_name = req.query.client_name as string || 'Guest';
     const clientIp = (req.headers['x-forwarded-for'] || req.socket.remoteAddress) as string;
-    console.log(req.headers['x-forwarded-for'])
-    console.log( req.ips)
-    const geo = geoip.lookup('197.210.226.168');
-    // console.log(geo)
-    // console.log(req.ip)
-
-
+    
+    
     const userHeaders = {
-        'Authorization': req.headers['authorization'], // Example: Authorization header
-        'User-Agent': req.headers['user-agent'], // Example: User-Agent header
+        'Authorization': req.headers['authorization'], 
+        'User-Agent': req.headers['user-agent'], 
         'X-forwarded-for': req.headers['x-forwarded-for']
-        // Add other headers as necessary
+       
     };
-    if (!geo) {
-        return res.status(500).json({ error: 'Unable to determine location' });
-    }
-
-    const city = geo.city || 'Unknown';
-    const location = `${city}`;
-    // const weatherUrl = `http://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${city}&aqi=no`
+   let location;
+   let temperature;
 
     try {
-        const ipResponse = await axios.get(`https://api.geoapify.com/v1/ipinfo?&apiKey=${weatherApiKey}`, {
+        const ipResponse = await axios.get(`https://api.geoapify.com/v1/ipinfo?&apiKey=${ipApiKey}`, {
             headers: userHeaders
          })
-        // const weatherResponse = await axios.get(weatherUrl);
+      
         if(ipResponse.data) {
-             console.log(ipResponse.data)
+            
+             const geo = geoip.lookup(`${ipResponse.data.ip}`);
+             if (geo) {
+                
+                location = geo.city || "unknown"
+               try {
+                    const weatherUrl = `http://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${geo.city}&aqi=no`
+                    const weatherData = await axios.get(weatherUrl)
+                    if (weatherData.data) {
+                        
+                temperature = weatherData.data.current.temp_c || "unknown"
+                const response = {
+                    client_ip: clientIp,
+                    location,
+                    greeting: `Hello, ${client_name}! The temperature is ${temperature} degrees Celsius in ${location}`
+                };
+        
+                res.json(response);
+                    }
+               } catch(error) {
+                res.status(500).json({
+                    message: 'Failed to get weather data',
+                })
+
+               }
+                
+             }  
         }
-        // const temperature = weatherResponse.data.main.temp;
 
-
-
-        // const response = {
-        //     client_ip: clientIp,
-        //     location,
-        //     greeting: `Hello, ${visitorName}! The temperature is ${temperature} degrees Celsius in ${location}`
-        // };
-
-        res.json({ipResponse:ipResponse.data, clientIp:clientIp });
     } catch (error) {
         res.status(500).json({
             message: 'Failed to get weather data',
@@ -66,3 +70,9 @@ app.get('/api/hello', async (req: Request, res: Response) => {
 app.listen(port, () => {
     console.log(`Server running at port ${port}`);
 });
+
+
+
+
+
+
